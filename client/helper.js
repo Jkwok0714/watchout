c7ffd827e2fc892ab3a753426a2c37f71dca6fd7
Justin Kwok <jkwok0714@gmail.com>
2017-09-05 14:33:51 -0700

    Score is kept track of, and helper functions relocated to reduce clutter

---
+++ client/helper.js
@@ -0,0 +1,76 @@
+function dragmove(d) {
+  var x = d3.event.x - 50;
+  var y = d3.event.y - 50;
+  d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+  increaseScore();
+}
+
+var generateStartAndEnd = function() {
+  var random = Math.floor(Math.random() * 4);
+  var start, end;
+  if (random === 0) {
+    //Start top, go bottom. Random X
+    start = [Math.random() * boardWidth, 0 - imgSize];
+    end = [Math.random() * boardWidth, boardHeight + imgSize];
+  } else if (random === 1) {
+    //Start left, go right Random Y
+    start = [0 - imgSize, Math.random() * boardHeight];
+    end = [boardWidth + imgSize, Math.random() * boardHeight];
+  } else if (random === 2) {
+    //Start bottom, go top Random X
+    start = [Math.random() * boardWidth, boardHeight + imgSize];
+    end = [Math.random() * boardWidth, 0 - imgSize];
+  } else {
+    //Start right, go left Random Y
+    start = [boardWidth + imgSize, Math.random() * boardHeight];
+    end = [0 - imgSize, Math.random() * boardHeight];
+  }
+  return [start, end];
+}
+
+var generateAsteroid = function() {
+  var result = {};
+  result.startPos, result.endPos = [];
+
+  [result.startPos, result.endPos] = generateStartAndEnd();
+  return result;
+}
+
+var getPos = function(d, request) {
+  if (request === 'start') {
+    return 'translate(' + d.startPos[0] + ',' + d.startPos[1] + ')';
+  } else {
+    return 'translate(' + d.endPos[0] + ',' + d.endPos[1] + ')';
+  }
+};
+
+
+//Collision detection
+var handleMouseOver = function(d, i) {
+  sampleData = [];
+  collisions += 1;
+
+  if (score > highScore) {
+    d3.select('.highscore span').text(score);
+    highScore = score;
+  }
+
+  //Reset scorescore = 0;
+  score = 0;
+  d3.select('.current span').text(score);
+
+  d3.select('.collisions span').text(collisions);
+  d3.selectAll('.asteroid').remove();
+  d3.select('.board .player').attr('transform', 'translate(' + boardWidth/2 + ',' + boardHeight/2 + ')');
+};
+
+var increaseScore = function() {
+  //scoreTimer = setInterval(() => {
+  score += 1;
+  d3.select('.current span').text(score);
+  //}, 500);
+};
+
+var stopIncreasingScore = function() {
+  clearInterval(scoreTimer);
+};
--- client/watchout.js
+++ client/watchout.js
@@ -17,92 +17,18 @@ var maxAsteroidsOnField = 7;
 var highScore = 0, collisions = 0, score = 0;
 var scoreTimer;
 
+window.alert('Keep dragging the Millenium Falcon to increase score. Game over when cursor hits asteroids!');
+
 var container = d3.select('.board').append('svg').attr('width', boardWidth).attr('height', boardHeight)
 .style('border', '1px solid yellow').attr('class', 'gameBoard');
 
 var drag = d3.behavior.drag()
 .on("drag", dragmove);
 
-function dragmove(d) {
-  var x = d3.event.x - 50;
-  var y = d3.event.y - 50;
-  d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
-}
-
 var player = container.append('svg:image').attr('xlink:href', 'falcon.png')
   .attr('height', imgSize * 1.5).attr('width', imgSize * 1.5).attr('class', 'player')
   .attr('transform', 'translate(' + boardWidth/2 + ',' + boardHeight/2 + ')')
   .call(drag);
-
-
-
-
-
-
-
-
-
-var generateStartAndEnd = function() {
-  var random = Math.floor(Math.random() * 4);
-  var start, end;
-  if (random === 0) {
-    //Start top, go bottom. Random X
-    start = [Math.random() * boardWidth, 0 - imgSize];
-    end = [Math.random() * boardWidth, boardHeight + imgSize];
-  } else if (random === 1) {
-    //Start left, go right Random Y
-    start = [0 - imgSize, Math.random() * boardHeight];
-    end = [boardWidth + imgSize, Math.random() * boardHeight];
-  } else if (random === 2) {
-    //Start bottom, go top Random X
-    start = [Math.random() * boardWidth, boardHeight + imgSize];
-    end = [Math.random() * boardWidth, 0 - imgSize];
-  } else {
-    //Start right, go left Random Y
-    start = [boardWidth + imgSize, Math.random() * boardHeight];
-    end = [0 - imgSize, Math.random() * boardHeight];
-  }
-  return [start, end];
-}
-
-var generateAsteroid = function() {
-  var result = {};
-  result.startPos, result.endPos = [];
-
-  [result.startPos, result.endPos] = generateStartAndEnd();
-  return result;
-}
-
-var getPos = function(d, request) {
-  if (request === 'start') {
-    return 'translate(' + d.startPos[0] + ',' + d.startPos[1] + ')';
-  } else {
-    return 'translate(' + d.endPos[0] + ',' + d.endPos[1] + ')';
-  }
-};
-
-
-//Collision detection
-var handleMouseOver = function(d, i) {
-  sampleData = [];
-  collisions += 1;
-  d3.select('.collisions span').text(collisions);
-  d3.selectAll('.asteroid').remove();
-  //window.alert('Mouse over!');
-};
-
-var increaseScore = function() {
-  //scoreTimer = setInterval(() => {
-  score += 5;
-  d3.select('.current span').text(score);
-  //}, 500);
-};
-
-var stopIncreasingScore = function() {
-  clearInterval(scoreTimer);
-};
-
-
 
 
 var runAsteroidSpawner = function() {
@@ -135,7 +61,7 @@ var runAsteroidSpawner = function() {
   //   score += 10;
   //   d3.select('.current span').text(score);
   // }
-  increaseScore();
+  // increaseScore();
 
   setTimeout(runAsteroidSpawner, 500);
 }
